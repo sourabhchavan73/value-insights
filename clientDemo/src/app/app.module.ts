230c68ae2ebb6aade02a0de5301e5144801de72f
sourabh.chavan <sourabh.chavan@webshar.org>
2023-04-13 17:27:02 +0530

    break issue fix

--- clientDemo/src/app/app.module.ts
+++ clientDemo/src/app/app.module.ts
@@ -1,27 +1,4 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CDHeaderComponent } from './cdheader/cdheader.component';
-import { ButtonModule } from 'primeng/button';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-@NgModule({
-  declarations: [
-    AppComponent,
-    CDHeaderComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ButtonModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
@@ -32,7 +9,6 @@ import { ButtonModule } from 'primeng/button';
 import { CdBodyComponent } from './cd-body/cd-body.component';
 import { NavtabComponent } from './navtab/navtab.component';
 import { TabViewModule } from 'primeng/tabview';
-import { MlrtHeaderComponent } from './mlrt-header/mlrt-header.component';
 
 @NgModule({
   declarations: [
@@ -40,7 +16,6 @@ import { MlrtHeaderComponent } from './mlrt-header/mlrt-header.component';
     CDHeaderComponent,
     CdBodyComponent,
     NavtabComponent,
-    MlrtHeaderComponent
   ],
   imports: [
     BrowserModule,
--- clientDemo/src/app/cdheader/cdheader.component.ts
+++ clientDemo/src/app/cdheader/cdheader.component.ts
@@ -1,13 +1,4 @@
-import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-cdheader',
-  templateUrl: './cdheader.component.html',
-  styleUrls: ['./cdheader.component.css']
-})
-export class CDHeaderComponent {
-
-}
 import { Component } from '@angular/core';
 
 
